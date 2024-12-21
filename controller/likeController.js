53111b5f969a6ff17503dfcccf5a3948f34254c2
Jini <wlsdl6685@gmail.com>
2024-12-21 11:46:10 +0900

    like 컨트롤러 추가

---
+++ controller/likeController.js
@@ -0,0 +1,15 @@
+const conn = require('../mariadb');
+const { StatusCodes } = require('http-status-codes');  // http-status-codes 모듈
+
+const addLike = (req, res) => {
+
+}
+
+const removeLike = (req,res)=>{
+
+}
+
+module.exports = {
+    addLike,
+    removeLike
+};
